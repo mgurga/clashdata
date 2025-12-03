e61d9662714a374d114e92d6c30a7669191ad6eb
mgurga <motiurga@gmail.com>
2025-12-03 11:42:00 -0800

    add more tags to cards

--- database/card_usage.py
+++ database/card_usage.py
@@ -78,80 +78,80 @@ card_usage = (
     .orderBy(F.desc("wins_with_card"))
 )
 
-# tags: WinCondition, Spell, MiniTank, AirDefense, Building, AoE
+# tags: WinCondition, Spell, MiniTank, AirDefense, Building, AoE, Swarm, Support, Tank, Cycle
 card_data = spark.createDataFrame(
     [
 [26000000, 3, "MiniTank,"], # Knight
 [26000001, 3, "AirDefense,"], # Archers
-[26000002, 2, ""], # Goblins
-[26000003, 5, "WinCondition,"], # Giant
-[26000004, 7, "WinCondition,"], # P.E.K.K.A
-[26000005, 3, ""], # Minions
+[26000002, 2, "Swarm,"], # Goblins
+[26000003, 5, "WinCondition,Tank,"], # Giant
+[26000004, 7, "WinCondition,Tank,"], # P.E.K.K.A
+[26000005, 3, "AirDefense,Swarm,"], # Minions
 [26000006, 5, "WinCondition,"], # Balloon
-[26000007, 5, "AoE,"], # Witch
-[26000008, 5, ""], # Barbarians
-[26000009, 8, "WinCondition,AoE,"], # Golem
-[26000010, 1, ""], # Skeletons
+[26000007, 5, "AoE,Support,"], # Witch
+[26000008, 5, "Swarm,"], # Barbarians
+[26000009, 8, "WinCondition,AoE,Tank,"], # Golem
+[26000010, 1, "Swarm,Cycle,"], # Skeletons
 [26000011, 4, "MiniTank,AoE,"], # Valkyrie
-[26000012, 3, ""], # Skeleton Army
+[26000012, 3, "Swarm,"], # Skeleton Army
 [26000013, 2, "AoE,"], # Bomber
 [26000014, 4, "AirDefense,"], # Musketeer
 [26000015, 4, "AirDefense,AoE,"], # Baby Dragon
-[26000016, 5, "WinCondition,"], # Prince
-[26000017, 5, "AirDefense,AoE,"], # Wizard
+[26000016, 5, "WinCondition,MiniTank,"], # Prince
+[26000017, 5, "AirDefense,AoE,Support,"], # Wizard
 [26000018, 4, "MiniTank,"], # Mini P.E.K.K.A
-[26000019, 2, "AirDefense,"], # Spear Goblins
-[26000020, 6, "WinCondition,"], # Giant Skeleton
+[26000019, 2, "AirDefense,Support,"], # Spear Goblins
+[26000020, 6, "WinCondition,Tank,"], # Giant Skeleton
 [26000021, 4, "WinCondition,"], # Hog Rider
-[26000022, 4, ""], # Minion Horde
-[26000023, 3, "AirDefense,AoE,"], # Ice Wizard
-[26000024, 6, "WinCondition,"], # Royal Giant
-[26000025, 3, ""], # Guards
-[26000026, 3, "AirDefense,AoE,"], # Princess
+[26000022, 4, "AirDefense,Swarm,"], # Minion Horde
+[26000023, 3, "AirDefense,AoE,Support,"], # Ice Wizard
+[26000024, 6, "WinCondition,Tank,"], # Royal Giant
+[26000025, 3, "Swarm,"], # Guards
+[26000026, 3, "AirDefense,AoE,Support,"], # Princess
 [26000027, 4, "MiniTank,AoE,"], # Dark Prince
 [26000028, 9, "AirDefense,"], # Three Musketeers
-[26000029, 7, ""], # Lava Hound
-[26000030, 1, "AoE,"], # Ice Spirit
-[26000031, 1, "AoE,"], # Fire Spirits
+[26000029, 7, "WinCondition,"], # Lava Hound
+[26000030, 1, "AoE,Cycle,"], # Ice Spirit
+[26000031, 1, "AoE,Cycle,"], # Fire Spirits
 [26000032, 3, "MiniTank,"], # Miner
 [26000033, 6, "WinCondition,"], # Sparky
-[26000034, 5, "AoE,"], # Bowler
-[26000035, 4, ""], # Lumberjack
-[26000036, 4, ""], # Battle Ram
-[26000037, 4, ""], # Inferno Dragon
+[26000034, 5, "AoE,Support,"], # Bowler
+[26000035, 4, "Support,MiniTank"], # Lumberjack
+[26000036, 4, "WinCondition,"], # Battle Ram
+[26000037, 4, "Support,"], # Inferno Dragon
 [26000038, 2, "MiniTank,AoE,"], # Ice Golem
-[26000039, 3, "AirDefense,"], # Mega Minion
-[26000040, 3, "AirDefense,"], # Dart Goblin
-[26000041, 3, "AirDefense,"], # Goblin Gang
-[26000042, 4, "AirDefense,AoE,"], # Electro Wizard
+[26000039, 3, "AirDefense,Support,"], # Mega Minion
+[26000040, 3, "AirDefense,Support,"], # Dart Goblin
+[26000041, 3, "AirDefense,Swarm,"], # Goblin Gang
+[26000042, 4, "AirDefense,AoE,Support,"], # Electro Wizard
 [26000043, 6, "WinCondition,"], # Elite Barbarians
 [26000044, 4, "AirDefense,AoE,"], # Hunter
-[26000045, 5, "AirDefense,AoE,"], # Executioner
-[26000046, 3, ""], # Bandit
-[26000047, 7, ""], # Royal Recruits
-[26000048, 4, ""], # Night Witch
-[26000049, 2, "AirDefense,"], # Bats
+[26000045, 5, "AirDefense,AoE,Support,"], # Executioner
+[26000046, 3, "Support,"], # Bandit
+[26000047, 7, "Swarm,"], # Royal Recruits
+[26000048, 4, "Support,"], # Night Witch
+[26000049, 2, "AirDefense,Swarm,Cycle,"], # Bats
 [26000050, 3, "AoE,"], # Royal Ghost
 [26000051, 5, "WinCondition,"], # Ram Rider
-[26000052, 4, "AirDefense,"], # Zappies
-[26000053, 5, ""], # Rascals
+[26000052, 4, "AirDefense,Support,"], # Zappies
+[26000053, 5, "MiniTank,"], # Rascals
 [26000054, 5, "MiniTank,"], # Cannon Cart
-[26000055, 7, "WinCondition,AoE,"], # Mega Knight
+[26000055, 7, "WinCondition,AoE,Tank,"], # Mega Knight
 [26000056, 3, "WinCondition,AoE,"], # Skeleton Barrel
 [26000057, 4, "AirDefense,"], # Flying Machine
 [26000058, 2, "WinCondition,"], # Wall Breakers
-[26000059, 5, ""], # Royal Hogs
-[26000060, 6, ""], # Goblin Giant
-[26000061, 3, ""], # Fisherman
+[26000059, 5, "WinCondition,"], # Royal Hogs
+[26000060, 6, "WinCondition,Tank,"], # Goblin Giant
+[26000061, 3, "Support,"], # Fisherman
 [26000062, 4, "AirDefense,AoE,"], # Magic Archer
 [26000063, 5, "AoE,"], # Electro Dragon
-[26000064, 3, "AirDefense,AoE,"], # Firecracker
-[26000067, 4, "WinCondition,"], # Elixir Golem
-[26000068, 4, "AoE,"], # Battle Healer
+[26000064, 3, "AirDefense,AoE,Support,"], # Firecracker
+[26000067, 4, "WinCondition,Tank,"], # Elixir Golem
+[26000068, 4, "AoE,Support,"], # Battle Healer
 [26000080, 4, "AirDefense,AoE,"], # Skeleton Dragons
 [26000083, 4, "AoE,"], # Mother Witch
-[26000084, 1, "AirDefense,AoE,"], # Electro Spirit
-[26000085, 7, "WinCondition,AoE,"], # Electro Giant
+[26000084, 1, "AirDefense,AoE,Cycle,"], # Electro Spirit
+[26000085, 7, "WinCondition,AoE,Tank,"], # Electro Giant
 [27000000, 3, "Building,"], # Cannon
 [27000001, 4, "Building,"], # Goblin Hut
 [27000002, 4, "Building,AoE,"], # Mortar
@@ -166,21 +166,21 @@ card_data = spark.createDataFrame(
 [27000012, 4, "Building,"], # Goblin Cage
 [28000000, 4, "Spell,AirDefense,AoE,"], # Fireball
 [28000001, 3, "Spell,AirDefense,AoE,"], # Arrows
-[28000002, 2, "Spell,AirDefense,AoE,"], # Rage
+[28000002, 2, "Spell,AirDefense,AoE,Cycle,"], # Rage
 [28000003, 6, "Spell,AirDefense,AoE,"], # Rocket
 [28000004, 3, "Spell,"], # Goblin Barrel
 [28000005, 4, "Spell,AirDefense,AoE,"], # Freeze
 [28000006, 0, "Spell,"], # Mirror
 [28000007, 6, "Spell,AirDefense,AoE,"], # Lightning
-[28000008, 2, "Spell,AirDefense,AoE,"], # Zap
+[28000008, 2, "Spell,AirDefense,AoE,Cycle,"], # Zap
 [28000009, 4, "Spell,AirDefense,AoE,"], # Poison
 [28000010, 5, "Spell,"], # Graveyard
-[28000011, 2, "Spell,AoE,"], # The Log
+[28000011, 2, "Spell,AoE,Cycle"], # The Log
 [28000012, 3, "Spell,AirDefense,AoE,"], # Tornado
 [28000013, 3, "Spell,AoE,"], # Clone
 [28000014, 3, "Spell,AoE,"], # Earthquake
 [28000015, 2, "Spell,AoE,"], # Barbarian Barrel
-[28000016, 1, "AoE,"], # Heal Spirit
+[28000016, 1, "AoE,Cycle,"], # Heal Spirit
 [28000017, 2, "Spell,AirDefense,AoE,"], # Giant Snowball
 [28000018, 3, "AirDefense,AoE,"], # Royal Delivery
     ],
